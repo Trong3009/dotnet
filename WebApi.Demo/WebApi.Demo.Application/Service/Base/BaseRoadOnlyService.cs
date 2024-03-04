e6f6d7eb3522a628e441b1b671bdca8552a9f124
Trong3009 <quytrong3009@gmail.com>
2024-03-04 17:33:29 +0700

    111

---
+++ WebApi.Demo/WebApi.Demo.Application/Service/Base/BaseRoadOnlyService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApi.Demo.Domain;
+
+namespace WebApi.Demo.Application
+{
+    public class BaseRoadOnlyService<TEntity,TKey, TEntityDto> : IReadOnlyService<TKey, TEntityDto> where TEntity : IEntity<TKey> where TEntityDto : class
+    {
+        protected readonly IReadOnlyRepository<TEntity, TKey> Repository;
+
+        public BaseRoadOnlyService(IReadOnlyRepository<TEntity, TKey> repository)
+        {
+            Repository = repository;
+        }
+
+        public Task<List<TEntityDto>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TEntityDto> GetAsync(TKey entityId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
